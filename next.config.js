131f593ea82f0bb62e883a9016e6f2699c641dc5
LeisureEnthusiast <68250137+LeisureEnthusiast@users.noreply.github.com>
2025-10-06 14:49:59 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [{
+      source: '/:path*',
+      headers: [
+        { key: 'Content-Security-Policy',
+          value: "frame-ancestors https://teams.microsoft.com https://*.teams.microsoft.com https://*.sharepoint.com 'self';" }
+      ],
+    }]
+  },
+}
+module.exports = nextConfig
